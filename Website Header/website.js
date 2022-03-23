4b91a5db874cb85a920bc1a42cd2a99a45e45c15
Chris Mbah <100575223+ChrisMbah@users.noreply.github.com>
2022-03-23 01:24:02 +0100

    Add files via upload

---
+++ Website Header/website.js
@@ -0,0 +1,6 @@
+
+let button=document.getElementById('button')
+let background=document.querySelector('.header')
+button.addEventListener('click',function(){
+    background.style.backgroundColor='black';
+})
